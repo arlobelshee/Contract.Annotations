a10f93aae8a0045859cb0c089fe5b71530954fcf
Arlo Belshee <arlo.belshee@live.com>
2014-03-30 13:38:01 -0700

    And just like that, I support .Net 2.0.

--- Contract.Annotations/Class1.cs
+++ Contract.Annotations/Class1.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Contract.Annotations
+﻿namespace Contract.Annotations
 {
-    public class Class1
-    {
-    }
+	public class Class1
+	{
+	}
 }
