89b841663cb7d6f6210b1fd69ba8fdfbf7da6d34
Arlo Belshee <arlo.belshee@live.com>
2014-04-03 20:28:29 -0700

    Creating brand new projects. I am organizing things differently and using wildcard shared-link files. Will work better with VS tooling...a little better.

---
+++ Contract.Annotations.net20/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Contract.Annotations.net20
+{
+    public class Class1
+    {
+    }
+}
---
+++ Contract.Annotations.portable-net45+sl5+wp8+windows8/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Contract.Annotations.portable_net45_sl5_wp8_windows8
+{
+    public class Class1
+    {
+    }
+}
