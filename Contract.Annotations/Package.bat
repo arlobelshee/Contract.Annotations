577fc9afa7656679d3f464f813df5d9207eb73d0
Arlo Belshee <arlo.belshee@live.com>
2014-04-03 21:39:13 -0700

    Can now generate the same package as before, but with the new project structure. Restructuring complete.

--- Contract.Annotations/Package.bat
+++ Contract.Annotations/Package.bat
@@ -1,3 +1,3 @@
 @echo off
 
-..\.nuget\nuget.exe pack -OutputDirectory ..\build -sym Contract.Annotations.Net45.csproj
+..\.nuget\nuget.exe pack -OutputDirectory ..\build -sym proj\Contract.Annotations.Net45.csproj
