2fc4faaf167ae6c505b46f1b1a31afa350006ee2
Arlo Belshee <arlo.belshee@live.com>
2014-04-03 21:12:45 -0700

    Cleaned up the assembly info files. Now sharing the core parts (everything that isn't frameworks specific).

---
+++ Contract.Annotations/src_net45/Properties/AssemblyInfoExtra.cs
@@ -0,0 +1,4 @@
+﻿using System.Runtime.InteropServices;
+
+[assembly: ComVisible(false)]
+[assembly: Guid("4d6b2083-57d9-4e77-a47b-6b5685c1e368")]
--- Contract.Annotations/src_shared/Properties/AssemblyInfo.cs
+++ Contract.Annotations/src_shared/Properties/AssemblyInfo.cs
@@ -1,30 +1,20 @@
 ﻿using System.Reflection;
-using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 
 [assembly: AssemblyTitle("Contract.Annotations")]
-[assembly: AssemblyDescription("	Annotations to declare the contracts in your code that support enhanced static analysis. This includes all the JetBrains.Annotations understood by any published version of ReSharper.")]
+[assembly:
+	AssemblyDescription(
+		"	Annotations to declare the contracts in your code that support enhanced static analysis. This includes all the JetBrains.Annotations understood by any published version of ReSharper."
+		)]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Arlo Belshee")]
 [assembly: AssemblyProduct("Contract.Annotations")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-#if !NO_COM
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-
-[assembly: Guid("f2f66238-0cf0-474d-a77a-a1969e3e81b6")]
-#endif
 
 // Version information for an assembly consists of the following four values:
 //
